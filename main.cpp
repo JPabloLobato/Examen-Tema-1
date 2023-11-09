0ec74982c9a1bde3e22148925692a7c26b3c0c2d
JPabloLobato <146821982+JPabloLobato@users.noreply.github.com>
2023-11-09 10:45:47 +0000

    2 commit

--- main.cpp
+++ main.cpp
@@ -1,2 +1,13 @@
 #include <iostream>
-#include <cmath>
+//Parte 2 - Ejercicio 1
+int main (){
+    int num1, num2, suma;
+    std::cout << "Ingrese el primer numero: ";
+    std::cin >> num1;
+    std::cout << "Ingrese el segundo numero: ";
+    std::cin >> num2;
+    suma = num1 + num2;
+    std::cout << "La suma de los numeros es: " << suma << std::endl;
+    return 0;
+}
+//Parte 2 - Ejercicio 2
